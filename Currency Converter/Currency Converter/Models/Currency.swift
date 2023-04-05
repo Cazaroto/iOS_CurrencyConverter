6b1d44e4aa284a4881bada76e46e623761d206d9
Danilo Cazaroto de Oliveira <danilo.cazaroto@lb.dev>
2023-04-05 10:02:07 -0300

    Layout formatted

---
+++ Currency Converter/Currency Converter/Models/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  Currency Converter
+//
+//  Created by Danilo Cazaroto de Oliveira on 28/03/23.
+//
+
+import Foundation
+
+struct Currency: Identifiable, Codable, Hashable{
+    var id: Int
+    let description: String
+}
